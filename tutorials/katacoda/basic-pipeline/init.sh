f1f0a0eb1acce0c75caad60cb11b55bab8cacf4e
Alan Greene <github.com@alangreene.net>
2021-06-01 10:39:33 +0100

    Update katacoda tutorials to use 'latest' releases of Pipelines and Dashboard
    Update the katacoda tutorials and playground to remove the hardcoded versions
    for both the Pipelines and Dashboard projects so it's easier to keep them
    up to date without requiring manual updates after each release.

--- tutorials/katacoda/basic-pipeline/init.sh
+++ tutorials/katacoda/basic-pipeline/init.sh
@@ -8,6 +8,6 @@ launch.sh
 kubectl apply --filename https://storage.googleapis.com/tekton-releases/pipeline/latest/release.yaml
 
 # Install the Tekton CLI
-curl -LO https://github.com/tektoncd/cli/releases/download/v0.10.0/tkn_0.10.0_Linux_x86_64.tar.gz
-sudo tar xvzf tkn_0.10.0_Linux_x86_64.tar.gz -C /usr/local/bin/ tkn
+curl -LO https://github.com/tektoncd/cli/releases/download/v0.18.0/tkn_0.18.0_Linux_x86_64.tar.gz
+sudo tar xvzf tkn_0.18.0_Linux_x86_64.tar.gz -C /usr/local/bin/ tkn
 
